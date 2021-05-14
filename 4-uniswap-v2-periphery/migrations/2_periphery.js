3b782d9c737684ad6c8c5034cdb981f070828a4f
Oleg <oleg.kubrakov@bgl.hk>
2021-05-14 20:59:39 +0800

    network

--- 4-uniswap-v2-periphery/migrations/2_periphery.js
+++ 4-uniswap-v2-periphery/migrations/2_periphery.js
@@ -1,10 +1,10 @@
 var UniswapV2Router02 = artifacts.require("UniswapV2Router02");
 var SigmaCoin = artifacts.require("SigmaCoin");
-const addressWETH9 = '0x0b4c433c6d00177aEbC1fAbC93299A80a932d12a';
-const addressFactory = '0x612CCeAce5392A8D7f64362975E8f95D8ec4BF83';
+const addressWETH9 = '0xc778417E063141139Fce010982780140Aa0cD5Ab';
+const addressFactory = '0xB989ff9fEe9C5906198E9d1dcE0E1361e12Cfa48';
 module.exports = function(deployer) {
   // deployment steps
   // deployer.deploy(UniswapV2Router01, addressFactory, addressWETH9, { gas: 670000000 });
   deployer.deploy(UniswapV2Router02, addressFactory, addressWETH9);
-  deployer.deploy(SigmaCoin);
+  // deployer.deploy(SigmaCoin);
 };
